65f3c525c1b6c83a7412b729817428d721d4c3df
Cybernaut <77c828fa@opayq.com>
2020-10-07 03:34:41 -0500

    Loading subscriptions from JSON file
    + Starting point: Subscriptions compt loads subs list from data.json

--- src/components/Subscriptions/Subscriptions.jsx
+++ src/components/Subscriptions/Subscriptions.jsx
@@ -1,12 +1,45 @@
 import style from "./Subscriptions.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import data from "../../common/data.json";
 
 function Subscriptions() {
+  let [subscriptions, setSubscriptions] = useState([]);
+
+  useEffect(() => {
+    let subsList = data.sources.map((subscription, index) => {
+      return (
+        <SubLink
+          key={index}
+          title={subscription.identifier.title}
+          link={subscription.link}
+        />
+      );
+    });
+    console.log(subsList);
+    setSubscriptions(subsList);
+  }, [data]);
+
   return (
-      <div id="subscriptions" className={style.subscriptions}>
-        <h1 className={style.header}>Subscriptions List</h1>
-      </div>
+    <div id="subscriptions" className={style.subscriptions}>
+      <h1 className={style.header}>Subscriptions</h1>
+      {subscriptions}
+    </div>
+  );
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  console.log("Button clicked! ", link);
+}
+
+class SubLink extends React.Component {
+  render() {
+    return (
+      <a href="#" onClick={handleClick}>
+        <h3>{this.props.title}</h3>
+      </a>
     );
+  }
 }
 
 export default Subscriptions;
