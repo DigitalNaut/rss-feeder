1168403a53e9407eabb5dfa0dd5a5992e67afc77
Cybernaut <77c828fa@opayq.com>
2020-10-08 08:49:24 -0500

    Minor fixes
    + Text formatting
    + Reverted root to div from body element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import { App } from './components/App.jsx'
 
-const root = document.getElementById('app');
+const root = document.getElementById('root');
 
 ReactDOM.render(<App />, root);
